200fed4bd3f7ff1f48b5b222bae5413ece8a724a
Luciano Larrea <takticflow@gmail.com>
2023-03-17 20:00:32 -0300

    main.py basico

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from fastapi import FastAPI
-import asyncio
+# import asyncio
 
 app = FastAPI(title='PI1',
             description='DTS-08')
@@ -10,19 +10,19 @@ app = FastAPI(title='PI1',
 async def index():
     return {'Hola' : 'Mundo'}
 
-@app.get('/max_duration/{year}/{platform}/{duration_type}')
-async def get_max_duration(year:int, platform:str, duration_type:str): 
-    All = pd.read_csv("data/all.csv")
-    Q1 = All[(All['platform'] == platform) & (All['release_year'] == year) & (All['duration_type'] == duration_type)].sort_values(by = 'duration_int' , ascending=False)
-    max_duration_title = Q1['title'].values[0]
-    return max_duration_title
-# get_max_duration(year, platform, duration_type)
+# @app.get('/max_duration/{year}/{platform}/{duration_type}')
+# async def get_max_duration(year:int, platform:str, duration_type:str): 
+#     All = pd.read_csv("data/all.csv")
+#     Q1 = All[(All['platform'] == platform) & (All['release_year'] == year) & (All['duration_type'] == duration_type)].sort_values(by = 'duration_int' , ascending=False)
+#     max_duration_title = Q1['title'].values[0]
+#     return max_duration_title
+# # get_max_duration(year, platform, duration_type)
 
-async def main():
-    max_duration_title = await get_max_duration(2014, 'amazon', 'min')
-    print(max_duration_title)
+# async def main():
+#     max_duration_title = await get_max_duration(2014, 'amazon', 'min')
+#     print(max_duration_title)
 
-asyncio.run(main())
+# asyncio.run(main())
 
     # # Encontrar la película con mayor duración
     # max_duration = All['duration_int'].max()
