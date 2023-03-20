d3892310df0b4bb303682bb88d99c1e829946eeb
Luciano Larrea <takticflow@gmail.com>
2023-03-20 02:34:22 -0300

    Update 5-Deploy.py

--- 5-Deploy.py
+++ 5-Deploy.py
@@ -12,7 +12,7 @@ import time
 
 # Crear un título para la aplicación
 st.set_page_config(page_title='Streaming Services') # Nombre para configurar la pagina web
-st.header('Consultas de títulos de películas y series') # Titulo de la pagina
+st.header('Consultas de títulos de streaming') # Titulo de la pagina
 
 # Cargar datos para consultas
 All = pd.read_csv("data/all.csv")
@@ -20,16 +20,8 @@ Score = pd.read_parquet('data/score.parquet')
 titles_list = All['title'].values
 
 st.balloons()
-progress_text = "Operación en progreso. Espere por favor."
-my_bar = st.progress(0, text=progress_text)
-for percent_complete in range(100):
-    time.sleep(0.1)
-    my_bar.progress(percent_complete + 1, text=progress_text)
+
 st.write('Sea paciente para la ejecución de las consultas')
-
-# with st.spinner('Espere...'):
-#     time.sleep(5)
-
 
 # Definir las funciones para las consultas
 # Query 1: Duracion maxima
@@ -77,22 +69,24 @@ def get_titles(year:int, platform:str, duration_type:str):
     #     return ("Not titles found with those parameters.")
 
 # Opciones de consulta
-options = ['Inicio','Duración máxima', 'Títulos por puntuación', 'Títulos por plataforma', 'Actor con más apariciones','Titulos recomendados']
-query = st.sidebar.selectbox('Seleccione una consulta', options)
+options = ['A-Inicio','B-Duración máxima', 'C-Títulos por puntuación', 'D-Títulos por plataforma', 'E-Actor con más apariciones','F-Titulos recomendados']
+# query0 = st.sidebar.selectbox('1-Seleccione una consulta', options)
+query = st.sidebar.radio('1-Seleccione el tipo de consulta',options)
 # usuario_id = st.sidebar.number_input('ID del usuario (Titulos recomendados)', min_value=1, max_value=270895)
 # choosed = st.sidebar.selectbox('Seleccione un titulo (Titulos recomendados)', titles_list)
 # st.sidebar.button('Recomendar')
 
-if query == 'Inicio':
-         st.write('Bienvenido a la aplicación de consultas en el catálogo de servicios de streaming')
+if query == 'A-Inicio':
+        st.write('Bienvenido a la aplicación de consultas en el catálogo de servicios de streaming')
+        st.write('Siga el orden numerico de cada campo para completar su consulta')
 
 # Consulta 1: Duración máxima
-if query == 'Duración máxima':
+if query == 'B-Duración máxima':
     st.subheader('Duración máxima por año y plataforma')
-    year = st.number_input('Año', min_value=1920, max_value=2023, value=2020, step=1)
-    platform = st.selectbox('Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
-    duration_type = st.selectbox('Tipo de duración', ['min', 'season'])
-    if st.button('Consultar'):
+    year = st.sidebar.number_input('2-Año', min_value=1920, max_value=2023, value=2020, step=1)
+    platform = st.sidebar.selectbox('3-Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
+    duration_type = st.sidebar.selectbox('4-Tipo de duración', ['min', 'season'])
+    if st.sidebar.button('Consultar'):
         result = get_max_duration(year, platform, duration_type)
         if isinstance(result, str):
             st.write(f'La duración máxima en {duration_type.lower()}s para {year} en {platform} es: {result}.')
@@ -100,48 +94,48 @@ if query == 'Duración máxima':
             st.write(result)
 
 # Consulta 2: Títulos por puntuación
-if query == 'Títulos por puntuación':
+if query == 'C-Títulos por puntuación':
     st.subheader('Títulos con una puntuación dada en una plataforma y año determinados')
-    platform = st.selectbox('Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
-    scored = st.number_input('Puntuación mínima', min_value=1.0, max_value=5.0, value=3.5, step=0.5)
-    year = st.number_input('Año', min_value=1920, max_value=2023, value=2020, step=1)
-    if st.button('Consultar'):
+    platform = st.sidebar.selectbox('2-Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
+    scored = st.sidebar.number_input('3-Puntuación mínima', min_value=1.0, max_value=5.0, value=3.5, step=0.5)
+    year = st.sidebar.number_input('4-Año', min_value=1920, max_value=2023, value=2020, step=1)
+    if st.sidebar.button('Consultar'):
         result = get_score_count(platform, scored, year)
         st.write(f'Hay {result} títulos en {platform} con una puntuación de {scored} o más en {year}.')
 
 # Consulta 3: Títulos por plataforma
-if query == 'Títulos por plataforma':
+if query == 'D-Títulos por plataforma':
     st.subheader('Número de títulos en una plataforma dada')
-    platform = st.selectbox('Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
-    if st.button('Consultar'):
+    platform = st.sidebar.selectbox('2-Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
+    if st.sidebar.button('Consultar'):
         result = get_count_platform(platform)
         st.write(f'Hay {result} títulos en {platform}.')
 
 
 
 # Consulta 4: Actor con más apariciones
-if query == 'Actor con más apariciones':
+if query == 'E-Actor con más apariciones':
     st.subheader('Actor con más apariciones en una plataforma y año determinados')
-    platform = st.selectbox('Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
-    year = st.number_input('Año', min_value=1920, max_value=2022, value=2020, step=1)
-    if st.button('Consultar'):
+    platform = st.sidebar.selectbox('2-Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
+    year = st.sidebar.number_input('3-Año', min_value=1920, max_value=2022, value=2020, step=1)
+    if st.sidebar.button('Consultar'):
         result = get_actor(platform, year)
         if isinstance(result, str):
             st.write(result)
         else:
-            st.write(f'El actor con más apariciones en {platform} en {year} es {result["actor"]}, con {result["appearances"]} apariciones.')
+            st.write(f'El/los actor/es con más apariciones en {platform} en {year} es {result["actor"].title()}, con {result["appearances"]} apariciones.')
 
 # Consulta 5: Titulos recomendados
-if query == 'Titulos recomendados':
+if query == 'F-Titulos recomendados':
     st.subheader('Lista de titulos')
-    year = st.number_input('Año', min_value=1920, max_value=2023, value=2020, step=1)
-    platform = st.selectbox('Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
-    duration_type = st.selectbox('Tipo de duración', ['min', 'season'])
-    with st.expander("Consultar"):
+    year = st.sidebar.number_input('2-Año', min_value=1920, max_value=2023, value=2020, step=1)
+    platform = st.sidebar.selectbox('3-Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
+    duration_type = st.sidebar.selectbox('4-Tipo de duración', ['min', 'season'])
+    with st.expander("5-Consultar"):
     # if st.button('Consultar'):
         result = get_titles(year, platform, duration_type)
-        choosed = st.selectbox('Seleccione un titulo', result)
-        usuario_id = st.number_input('ID del usuario (Titulos recomendados)', min_value=1, max_value=270895)
+        choosed = st.selectbox('6-Seleccione un titulo', result)
+        usuario_id = st.number_input('7-ID del usuario (Titulos recomendados)', min_value=1, max_value=270895)
         if st.button('Recomendar'):
 
         
@@ -179,6 +173,12 @@ if query == 'Titulos recomendados':
             recomendaciones_usuario = recomendaciones_usuario.sort_values('Estimate_Score', ascending=False)
             st.write('Otros títulos recomendados que el usuario',usuario_id, 'no ha visto.')
             st.dataframe(recomendaciones_usuario)
+
+progress_text = "Operación en progreso. Espere por favor."
+my_bar = st.progress(0, text=progress_text)
+for percent_complete in range(100):
+    time.sleep(0.05)
+    my_bar.progress(percent_complete + 1, text=progress_text)
 
 with st.spinner('Solo un poco mas...'):
     time.sleep(1)
