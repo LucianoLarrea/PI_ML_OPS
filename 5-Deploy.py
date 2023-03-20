4f2408f41c2674b92cc1a482110e6ccf4265ce24
Luciano Larrea <takticflow@gmail.com>
2023-03-20 01:35:55 -0300

    Update 5-Deploy.py

--- 5-Deploy.py
+++ 5-Deploy.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 from surprise import Dataset, Reader, SVD
 from surprise.model_selection import train_test_split
-# import pickle
+import time
 
 # Cargar modelo entrenado desde el archivo
 # with open('model.pkl', 'rb') as file:
@@ -13,11 +13,23 @@ from surprise.model_selection import train_test_split
 # Crear un título para la aplicación
 st.set_page_config(page_title='Streaming Services') # Nombre para configurar la pagina web
 st.header('Consultas de títulos de películas y series') # Titulo de la pagina
-st.subheader('Sistema de consultas')
+
 # Cargar datos para consultas
 All = pd.read_csv("data/all.csv")
 Score = pd.read_parquet('data/score.parquet')
 titles_list = All['title'].values
+
+st.balloons()
+progress_text = "Operación en progreso. Espere por favor."
+my_bar = st.progress(0, text=progress_text)
+for percent_complete in range(100):
+    time.sleep(0.1)
+    my_bar.progress(percent_complete + 1, text=progress_text)
+st.write('Sea paciente para la ejecución de las consultas')
+
+# with st.spinner('Espere...'):
+#     time.sleep(5)
+
 
 # Definir las funciones para las consultas
 # Query 1: Duracion maxima
@@ -25,6 +37,7 @@ def get_max_duration(year:int, platform:str, duration_type:str):
     Q1 = All[(All['platform'] == platform) & (All['release_year'] == year) & (All['duration_type'] == duration_type)].sort_values(by = 'duration_int' , ascending=False)
     if len(Q1) > 0:
         titles_list = Q1['title']   # Observar cuidadosamente que se actualice
+        choosed = st.sidebar.selectbox('Seleccione un titulo (Titulos recomendados)', titles_list) # Prueba
         max_duration_title = Q1['title'].values[0]
         return max_duration_title
     else: 
@@ -63,14 +76,11 @@ def get_titles(year:int, platform:str, duration_type:str):
     # else: 
     #     return ("Not titles found with those parameters.")
 
-
-
-
 # Opciones de consulta
 options = ['Inicio','Duración máxima', 'Títulos por puntuación', 'Títulos por plataforma', 'Actor con más apariciones','Titulos recomendados']
 query = st.sidebar.selectbox('Seleccione una consulta', options)
-usuario_id = st.sidebar.number_input('ID del usuario (Titulos recomendados)', min_value=1, max_value=270895)
-choosed = st.sidebar.selectbox('Seleccione un titulo (Titulos recomendados)', titles_list)
+# usuario_id = st.sidebar.number_input('ID del usuario (Titulos recomendados)', min_value=1, max_value=270895)
+# choosed = st.sidebar.selectbox('Seleccione un titulo (Titulos recomendados)', titles_list)
 # st.sidebar.button('Recomendar')
 
 if query == 'Inicio':
@@ -85,7 +95,7 @@ if query == 'Duración máxima':
     if st.button('Consultar'):
         result = get_max_duration(year, platform, duration_type)
         if isinstance(result, str):
-            st.write(f'La duración máxima en {duration_type.lower()}s en {year} en {platform} es: {result}.')
+            st.write(f'La duración máxima en {duration_type.lower()}s para {year} en {platform} es: {result}.')
         else:
             st.write(result)
 
@@ -127,43 +137,49 @@ if query == 'Titulos recomendados':
     year = st.number_input('Año', min_value=1920, max_value=2023, value=2020, step=1)
     platform = st.selectbox('Seleccione una plataforma', ['amazon','disney','hulu','netflix'])
     duration_type = st.selectbox('Tipo de duración', ['min', 'season'])
-    if st.button('Consultar'):
+    with st.expander("Consultar"):
+    # if st.button('Consultar'):
         result = get_titles(year, platform, duration_type)
-        # choosed = st.sidebar.selectbox('Seleccione un titulo', result)
-        
-if st.sidebar.button('Recomendar'):
+        choosed = st.selectbox('Seleccione un titulo', result)
+        usuario_id = st.number_input('ID del usuario (Titulos recomendados)', min_value=1, max_value=270895)
+        if st.button('Recomendar'):
 
         
-# Cargar datos para el sistema de recomendacion
-    df1 = pd.read_parquet(('data/merge.parquet'))
-    df_title = pd.read_csv('data/df_title.csv')
+        # Cargar datos para el sistema de recomendacion
+            df1 = pd.read_parquet(('data/merge.parquet'))
+            df_title = pd.read_csv('data/df_title.csv')
 
-    # Configurar modelo de recomendación
-    reader = Reader()
-    N_filas = 10000 # Limitamos el dataset a N_filas
-    data = Dataset.load_from_df(df1[['userId', 'movieId', 'score']][:N_filas], reader)
-        #     
-    idx = df_title[df_title['title'] == choosed].index[0]
-    movieId = df_title.loc[idx, 'movieId']
-    # prediction = model.predict(usuario_id, movieId)
-    # Dividir dataset en entrenamiento y prueba
-    trainset, testset = train_test_split(data, test_size=.25)
-    # Entrenar modelo SVD
-    model = SVD()
-    model.fit(trainset) 
-    predictions = model.test(testset)
-    
-    
-    prediction = model.predict(usuario_id, movieId)
-    recomendacion = int(prediction.est * 20)
-    # Mostrar la predicción en Streamlit
-    st.sidebar.write('Este título es', recomendacion,'% para el usuario,',usuario_id)
-    
-    # Agrega un dataframe adicional con las puntuaciones de usuario
-    recomendaciones_usuario = df_title[['movieId','title']] # Todos los titulos
-    # Debemos extraer las películas que ya ha visto
-    usuario_vistas = df1[df1['userId'] == usuario_id] # Filtro por peliculas que el usuario califico
-    recomendaciones_usuario.drop(usuario_vistas.movieId, inplace = True,  errors='ignore') # Elimina las peliculas vistas
-    recomendaciones_usuario['Estimate_Score'] = recomendaciones_usuario['movieId'].apply(lambda x: model.predict(usuario_id, x).est)
-    recomendaciones_usuario = recomendaciones_usuario.sort_values('Estimate_Score', ascending=False)
-    st.dataframe(recomendaciones_usuario)
+            # Configurar modelo de recomendación
+            reader = Reader()
+            N_filas = 10000 # Limitamos el dataset a N_filas
+            data = Dataset.load_from_df(df1[['userId', 'movieId', 'score']][:N_filas], reader)
+                #     
+            idx = df_title[df_title['title'] == choosed].index[0]
+            movieId = df_title.loc[idx, 'movieId']
+            # prediction = model.predict(usuario_id, movieId)
+            # Dividir dataset en entrenamiento y prueba
+            trainset, testset = train_test_split(data, test_size=.25)
+            # Entrenar modelo SVD
+            model = SVD()
+            model.fit(trainset) 
+            predictions = model.test(testset)
+            
+            
+            prediction = model.predict(usuario_id, movieId)
+            recomendacion = int(prediction.est * 20)
+            # Mostrar la predicción en Streamlit
+            st.write('Este título es', recomendacion,'% para el usuario,',usuario_id)
+            
+            # Agrega un dataframe adicional con las puntuaciones de usuario
+            recomendaciones_usuario = df_title[['movieId','title']] # Todos los titulos
+            # Debemos extraer las películas que ya ha visto
+            usuario_vistas = df1[df1['userId'] == usuario_id] # Filtro por peliculas que el usuario califico
+            recomendaciones_usuario.drop(usuario_vistas.movieId, inplace = True,  errors='ignore') # Elimina las peliculas vistas
+            recomendaciones_usuario['Estimate_Score'] = recomendaciones_usuario['movieId'].apply(lambda x: model.predict(usuario_id, x).est)
+            recomendaciones_usuario = recomendaciones_usuario.sort_values('Estimate_Score', ascending=False)
+            st.write('Otros títulos recomendados que el usuario',usuario_id, 'no ha visto.')
+            st.dataframe(recomendaciones_usuario)
+
+with st.spinner('Solo un poco mas...'):
+    time.sleep(1)
+st.success('Listo!', icon="✅")
